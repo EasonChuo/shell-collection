204f552a814f0974c367b6b0fdc3005ebafb7652
Eason <eason@wezoomtek.com.tw>
2016-02-22 15:24:39 +0800

    add install nginx script for CentOS

---
+++ install-nginx.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#安裝nginx指令包
+#2016.02.22 init by Eason
+yum update
+yum install gcc* -y
+yum install pcre -y
+
+mkdir /usr/src
+cd /usr/src
+
+#下載指定版本的nginx
+export nginxVersion="1.9.11" 
+wget http://nginx.org/download/nginx-$nginxVersion.tar.gz 
+tar -xzf nginx-$nginxVersion.tar.gz 
+ln -sf nginx-$nginxVersion nginx
+
+#編譯nginx
+cd nginx 
+./configure \
+ --user=nginx \
+ --group=nginx \
+ --prefix=/etc/nginx \
+ --sbin-path=/usr/sbin/nginx \
+ --conf-path=/etc/nginx/nginx.conf \
+ --pid-path=/var/run/nginx.pid \
+ --lock-path=/var/run/nginx.lock \
+ --error-log-path=/var/log/nginx/error.log \
+ --http-log-path=/var/log/nginx/access.log \
+ --with-http_gzip_static_module \
+ --with-http_stub_status_module \
+ --with-http_ssl_module \
+ --with-pcre \
+ --with-file-aio \
+ --with-http_realip_module \
+ --without-http_scgi_module \
+ --without-http_uwsgi_module \
+ --without-http_fastcgi_module
+
+make
+make install
+useradd -r nginx
+
+#下載服務設定檔
+cd /etc/init.d
+wget -O /etc/init.d/nginx https://gist.github.com/sairam/5892520/raw/b8195a71e944d46271c8a49f2717f70bcd04bf1a/etc-init.d-nginx
+ 
+echo "Finish!"
